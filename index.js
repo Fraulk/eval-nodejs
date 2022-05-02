a7a67c59f174070c4c136b920d14c9f138ad9740
Frat Ulker <frat@cleanmob.eu>
2022-05-02 15:43:53 +0200

    Ajout de fichier html et lecture de ces fichier lors des requêtes

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const http = require("http");
+const fs = require("fs")
+const path = require("path")
 
 const server = http.createServer((req, res) => {
   try {
     console.log(req.httpVersion, req.url, req.method);
     if (req.url === "/" && req.method == "GET") {
       res.writeHead(200, { "content-type": "text/html" });
-      res.write("<h1>HELLO WORLD FRAT ULKER</h1>");
+      res.write(fs.readFileSync(path.join("./public/pages/index.html"), {encoding: 'utf-8'}));
       res.end();
     } else if (req.url == "/" && req.method != "GET") {
       res.writeHead(405, { "content-type": "text/html" });
-      res.write(`<h1>405: Méthode non autorisée</h1>`);
+      res.write(fs.readFileSync(path.join("./public/pages/method-not-allowed.html"), {encoding: 'utf-8'}));
       res.end();
     } else {
       res.writeHead(404, { "content-type": "text/html" });
-      res.write(`<h1>404: Page introuvable</h1>`);
+      res.write(fs.readFileSync(path.join("./public/pages/not-found.html"), {encoding: 'utf-8'}));
       res.end();
     }
   } catch (err) {
+    console.log(err)
     res.writeHead(500, { "content-type": "text/html" });
     res.write("<h1>500 Erreur interne au serveur</h1>");
     res.end();
